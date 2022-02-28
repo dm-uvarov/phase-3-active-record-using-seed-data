4554c4991be30ce03af5fb5976a3f56666f353f9
Jimmy <dm.a.uvarov@gmail.com>
2022-02-28 16:42:55 -0500

    made*

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,28 @@
-# Add a console message so we can see output when the seed file runs
+# should clears terminal
+if RUBY_PLATFORM =~ /win32|win64|\.NET|windows|cygwin|mingw32/i
+    system('cls')
+  else
+    system('clear')
+end
+
+
+print "Generating some data here["
+
+10.times { 
+    # imitating synchronious seeding
+    sleep 0.5
+    print "."
+    5.times { 
+        
+        Game.create(
+            title:      Faker::Game.title,
+            genre:      Faker::Game.genre,
+            platform:   Faker::Game.platform,
+            price:      rand(0..60)
+        )
+
+        }
+    }
+puts "]"
+sleep 1
+puts "Done."
